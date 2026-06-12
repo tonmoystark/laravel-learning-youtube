d14c7b4b3983b208d7a7088871c070a8222af958
tonmoystark <tonmoy.a009@gmail.com>
2026-06-12 18:36:06 +0600

    learning the basics

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,43 @@ use App\Http\Controllers\MessageController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [CardController::class, 'index']);
+Route::get('/chirper', [CardController::class, 'index']);
 
 Route::get('/welcome', [WelcomeController::class, 'welcome']);
 Route::get('/profile/{id}', [CardController::class, 'show']);
 Route::get('/chirper', [MessageController::class, 'index']);
+
+
+Route::get('/', function () {
+    return 'this is the home page';
+});
+
+Route::prefix('/details')->group(function () {
+    Route::get('/students', function () {
+        return 'getting students data';
+    })->name('student-details');
+
+    Route::get('/teachers', function () {
+        return 'getting teachers data';
+    })->name('teacher-details');
+});
+
+Route::fallback(function () {
+    return 'this page does not exists';
+});
+
+Route::get('/student/{id}/{registration}', function ($id, $registration) {
+    return 'student id ' . $id . ' registration number is ' . $registration;
+});
+
+// Route::get('views', function () {
+//     $name = 'testing';
+//     $email = 'test@t.bd';
+//     // return view('testerview')->with('name', $name)->with('email', $email);
+//     // return view('testerview', compact('name', 'email'));
+//     return view('testerview', ['name' => $name, 'email' => $email]);
+// });
+
+Route::get('luchu/{name}/{email}', function ($name, $email) {
+    return view('testerview', compact('name', 'email'));
+});
