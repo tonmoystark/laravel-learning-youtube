af1fa82692622763a0ab09b051b9ef5dfe41fc85
tonmoystark <tonmoy.a009@gmail.com>
2026-06-14 18:31:25 +0600

    learning migration

---
+++ database/migrations/2026_06_13_180734_add_date_of_birth.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('demo', function (Blueprint $table) {
+            //
+            $table->enum('gender', ['m', 'f'])->default('m');
+            $table->date('date_of_birth')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('demo', function (Blueprint $table) {
+            //
+            $table->dropColumn('date_of_birth');
+            $table->dropColumn('gender');
+        });
+    }
+};
