17fb6903e88de2fd1a90036cababbadb5f34bf82
tonmoystark <tonmoy.a009@gmail.com>
2026-06-15 18:07:42 +0600

    learned factory and seed

---
+++ database/factories/TeacherFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Teacher;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Teacher>
+ */
+class TeacherFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name'=>$this->faker->name(),
+            'email'=>$this->faker->safeEmail(),
+            'age'=>$this->faker->numberBetween(15, 55),
+        ];
+    }
+}
