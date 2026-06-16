3d44f748f718203b07cf6b94f4a27698a2f4da5e
tonmoystark <tonmoy.a009@gmail.com>
2026-06-16 18:18:26 +0600

    eloquent ORM completed

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,44 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Type\Integer;
 
 class StudentController extends Controller
 {
     //
     public function addData()
     {
-        DB::table('students')->insert([
-            [
-                'name' => 'Tonmoy',
-                'email' => 'tonmoy.a009@gmail.com',
-                'age' => 25,
-                'gender' => 'm',
-                'date_of_birth' => '2001-03-31'
-            ],
-            [
-                'name' => 'Atif',
-                'email' => 'atif.a009@gmail.com',
-                'age' => 28,
-                'gender' => 'm',
-                'date_of_birth' => '2001-01-01'
-            ],
-            [
-                'name' => 'Rakib',
-                'email' => 'rakib.a009@gmail.com',
-                'age' => 22,
-                'gender' => 'm',
-                'date_of_birth' => '2004-01-01'
-            ],
-        ]);
+
+        $data = new Student();
+        $data->name = 'Eloquent';
+        $data->email = 'eloquentData@try.com';
+        $data->age = 25;
+        $data->gender = 'M';
+        $data->date_of_birth = '2001-03-31';
+        $data->save();
+
+
+        // DB::table('students')->insert([
+        //         'name' => 'Tonmoy',
+        //         'email' => 'tonmoy.a009@gmail.com',
+        //         'age' => 25,
+        //         'gender' => 'm',
+        //         'date_of_birth' => '2001-03-31'  
+        // ]);
         return 'student data added';
     }
+
     public function showData()
     {
-        $data = DB::table('students')
-            ->where('age', '>', 22)
-            ->where('age', '<', 28)
+        // $data = DB::table('students')
+        //     ->where('age', '>', 22)
+        //     ->where('age', '<', 28)
+        //     ->first();
+
+        $data = Student::select('id',  'name')
+            ->where('id', 8)
             ->first();
 
         return $data;
@@ -47,20 +48,28 @@ class StudentController extends Controller
 
     public function updateData()
     {
-        DB::table('students')
-            ->where('id', 1)
-            ->update([
-                'name' => 'Changed Tonmoy',
-                'email' => 'iyadfajar@gmail.com',
-            ]);
-        return 'Data Updated';
+        // DB::table('students')
+        //     ->where('id', 1)
+        //     ->update([
+        //         'name' => 'Changed Tonmoy',
+        //         'email' => 'iyadfajar@gmail.com',
+        //     ]);
+
+        // lets do in eloquent way
+
+        $data = Student::where('id', 8);
+        $data->update([
+            'name' => 'one more change'
+        ]);
+
+        return 'data updated';
     }
 
     public function deleteData(int $id)
     {
-        DB::table('students')
-            ->where('id', $id)
-            ->delete();
-        return 'Data Deleted';
+        $data = Student::findOrFail($id);
+
+        $data->delete();
+        return 'data deleted';
     }
 }
